16ca4d86b4121881e1c15d243edeb435e1f946c0
markmercado1 <markemerson1245@gmail.com>
2026-04-22 20:22:47 -0500

    cambios

---
+++ ms-regitro-service/src/main/java/com/example/msregitroservice/MsRegitroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.msregitroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsRegitroServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsRegitroServiceApplication.class, args);
+    }
+
+}
---
+++ ms-regitro-service/src/test/java/com/example/msregitroservice/MsRegitroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.msregitroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsRegitroServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
